8146ec29a6811362414920025a97139e721848f3
Luis Galocha <galdomluis@gmail.com>
2020-03-04 22:38:15 +0100

    añade ejercicios 5 y 6, el 6 no está bien del todo

--- practica-01.py
+++ practica-01.py
@@ -169,7 +169,7 @@ def psr_backtraking(psr):
 
 mapa = coloreado_mapa(provincias,colores)
 reinas = n_reinas(4)
-print((psr_backtraking(n_reinas(4))))
+#print((psr_backtraking(n_reinas(4))))
     
 def psr_backtraking_fc_mrv(psr):
 
@@ -355,7 +355,8 @@ def AC3(psr,doms):
     return doms
 psr_n4=n_reinas(4)
 dominios = {1:[2,4],2:[1,2,3,4],3:[1,2,3,4],4:[1,2,3,4]}
-print(AC3(psr_n4, dominios))
+dominios = {1:[1,2,3,4],2:[3,4],3:[1,4],4:[1,2,3,4]}
+#print(AC3(psr_n4, dominios))
 
 
 # ===================================================================
@@ -386,7 +387,22 @@ print(AC3(psr_n4, dominios))
 # ({1: [2], 2: [1, 4], 3: [1, 3], 4: [1, 3, 4]}, {1: [4], 2: [1, 4], 3: [1, 3], 4: [1, 3, 4]})
 
 
+def parte_dominios(doms):
+    for x in doms:
+        if len(doms[x]) > 1:
+            aux1 = doms.copy()
+            aux1[x] = doms[x][0:int(len(doms[x])/2)]
+            aux2 = doms.copy()
+            aux2[x] = doms[x][int(len(doms[x])/2):len(doms[x])]
+            sol = (aux1,aux2)
+            break
+    return tuple(sol) 
 
+doms4_1={1: [2, 4], 2: [1, 4], 3: [1, 3], 4: [1, 3, 4]}
+#print(parte_dominios(doms4_1))
+
+# ({1: [2], 2: [1, 4], 3: [1, 3], 4: [1, 3, 4]}, {1: [4], 2: [1, 4], 3: [1, 3], 4: [1, 3, 4]})
+    
 
 
 
@@ -408,9 +424,24 @@ print(AC3(psr_n4, dominios))
 # >>> busqueda_AC3(psr_nreinas3)
 # No hay solución
 
-
-
-
+def busqueda_AC3(psr):
+    doms = psr.dominios.copy()
+    abiertos = [doms]
+    actual = abiertos[0]
+    while(abiertos != []):
+        r = AC3(psr,actual)
+        if any(len(r[x]) == 0 for x in r):
+            return "Sorry, has pifiao"
+        elif all(len(r[x]) == 1 for x in r):
+            return "Ya tienes solución chato y esta es: " + r
+        else:
+            s = parte_dominios(doms)
+            abiertos.append(s[0])
+            abiertos.append(s[1])
+            del abiertos[0]
+    return "Sorry, has pifiao"
+psr_nreinas4=n_reinas(4)
+print(busqueda_AC3(psr_nreinas4))
 
 
 # ===================================================================
